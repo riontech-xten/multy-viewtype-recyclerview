87af244da3d4ad4dd047615eebeb19d24e622ab3
Mithun <vaghela.mithun@gmail.com>
2025-11-29 12:17:39 +0530

    databinding error resolved

--- features/asiacup/src/main/java/com/xtensolutions/asiacup/presentation/ui/activity/AsiaCupActivity.kt
+++ features/asiacup/src/main/java/com/xtensolutions/asiacup/presentation/ui/activity/AsiaCupActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.xtensolutions.asiacup.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * Created by Vaghela Mithun R. on 28-11-2025 - 20:17.
  * Email : vaghela.mithun@gmail.com
  * Mob   : +919727206702
  **/
+@AndroidEntryPoint
 abstract class AsiaCupActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
--- interfacesample/build.gradle.kts
+++ interfacesample/build.gradle.kts
@@ -34,6 +34,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
 }
 
 dependencies {
